4a98c56855441c50c2ec4e2ad198a04beb96f317
Duy <dieneasia107@gmail.com>
2025-11-13 18:47:42 +0700

    Fix: Add dynamic export to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
   const router = useRouter()
   const searchParams = useSearchParams()
